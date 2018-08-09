6f511b995e2d7680aa9003d38636cc3e4c8d84b4
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-09 10:06:27 +0200

    Import required libs

--- assets/jsx/base.jsx
+++ assets/jsx/base.jsx
@@ -2,14 +2,29 @@ const $ = require('jquery');
 // create global $ and jQuery variables
 global.$ = global.jQuery = $;
 
-require('bootstrap-sass')
-require('startbootstrap-sb-admin/js/sb-admin.min.js')
-// require('jquery-ui-dist')
+require('bootstrap-sass');
+require('bootstrap-select');
+require('bootstrap/dist/js/bootstrap.min')
+require('startbootstrap-sb-admin-2/dist/js/sb-admin-2.js');
+require('metismenu/dist/metisMenu.js');
+
+// require('bootstrap-confirmation2/src/confirmation.js');
+
+require('datatables.net');
+require('datatables.net-bs');
+
+require('datatables.net-buttons-bs');
+require('datatables.net-buttons/js/buttons.colVis');
+
+require('jquery/dist/jquery.min');
+require('jquery-ui-dist/jquery-ui');
+require('plotly.js/dist/plotly.min');
 
 $('document').ready(function(){
     $('#loginButton').on('click', function() {
         var username = $('#inputUsername').val()
         var password = $('#inputPassword').val()
+        console.log(username)
         $.ajax({
             method: "POST",
             url: "/login",
@@ -18,7 +33,7 @@ $('document').ready(function(){
                 password: password
             },
             success: function(result){
-                console.log(result)
+                window.location.href = '/login'
             }
         })
     })
