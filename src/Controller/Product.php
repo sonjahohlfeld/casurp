1aa655aa543d39d4586d59ae5ee888c0e859649b
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-13 15:04:13 +0200

    Implement functionality for listing products

--- src/Controller/Product.php
+++ src/Controller/Product.php
@@ -15,9 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class Product extends Controller
 {
     /**
-     * @Route("/products")
+     * @Route("/products", name="products")
      */
-    public function displayProductsAction(){
-        return $this->render("product.html.twig");
+    public function displayProductsAction(\App\Service\Listing\Product $product){
+        $result = $product->execute();
+        return $this->render("product.html.twig", [
+            'products' => $result
+        ]);
     }
 }
