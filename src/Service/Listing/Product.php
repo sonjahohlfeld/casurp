b0dbb2d858f99438f7212e40aa408114c46fc457
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-13 15:03:46 +0200

    Create new service for listing products

---
+++ src/Service/Listing/Product.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sonja
+ * Date: 13.08.18
+ * Time: 14:11
+ */
+
+namespace App\Service\Listing;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class Product {
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->em = $entityManager;
+    }
+
+    public function execute(){
+        return $this->em->getRepository(\App\Entity\Product::class)->findAll();
+    }
+}
