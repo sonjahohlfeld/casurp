bccd4b0ea5733cf692546e4918c9c8ddbeb3b019
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-14 16:18:53 +0200

    Add simple web service for testing mm webhooks

--- src/Controller/Product.php
+++ src/Controller/Product.php
@@ -11,6 +11,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class Product extends Controller
 {
@@ -22,5 +23,18 @@ class Product extends Controller
         return $this->render("product.html.twig", [
             'products' => $result
         ]);
+    }
+
+    /**
+     * @Route("/products/milk", name="count_of_milk")
+     * @return Response
+     */
+    public function getCountOfMilk(\App\Service\Listing\Product $product){
+        $response = new Response();
+        $response->setContent(json_encode(array(
+            'data' => 123,
+        )));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
