082a4fe75053a70b6f2548479b597352e5df39d8
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-29 13:34:48 +0200

    Write service for removing products

--- src/Service/Edit/RemoveProduct.php
+++ src/Service/Edit/RemoveProduct.php
@@ -6,7 +6,27 @@
  * Time: 13:04
  */
 
+namespace App\Service\Edit;
+
 class RemoveProduct
 {
+    private $em;
 
+    public function __construct(\Doctrine\ORM\EntityManagerInterface $entityManager){
+        $this->em = $entityManager;
+    }
+
+    public function execute($product){
+        $result = array();
+        $p = $this->em->getRepository(\App\Entity\Product::class)->findOneBy(array(
+            'name' => $product
+        ));
+        if($p === null){
+            $result['error'] = "There exist no product with the name ".$product;
+        } else {
+            $this->em->remove($p);
+            $result['success'] = "Successfully remove product ".$product;
+        }
+        return $result;
+    }
 }
