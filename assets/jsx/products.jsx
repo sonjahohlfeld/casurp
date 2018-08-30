4cd82da8cd4d45710268907450f178af0316d938
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-30 09:10:37 +0200

    Update jsx file for products

--- assets/jsx/products.jsx
+++
@@ -1,9 +0,0 @@
-$('document').ready(function(){
-    $('#removeProductButton-Sugar').on('click', function(){
-        console.log("test")
-    })
-
-    $('#createNewProduct').on('click', function(){
-        $('#exampleModal').modal()
-    })
-})
