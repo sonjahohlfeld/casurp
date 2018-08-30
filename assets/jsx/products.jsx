f9ab34a81a87f97c6b59d838f80f05fb2e25b381
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-08-30 09:59:54 +0200

    Add callback function for handling modal

---
+++ assets/jsx/products.jsx
@@ -0,0 +1,7 @@
+$(function () {
+    $(document).on('click', '#createNewProductConfirm', function(event){
+        var name = $('#createNewProduct_name').val()
+        var count = $('#createNewProduct_count').val()
+        var price = $('#createNewProduct_price').val()
+    })
+})
