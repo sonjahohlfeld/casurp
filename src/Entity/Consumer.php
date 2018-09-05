65f97ac94f4e54cf1073a92e75438fb4a1865db4
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-09-05 13:07:51 +0200

    Add new consumer entity

---
+++ src/Entity/Consumer.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ConsumerRepository")
+ */
+class Consumer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $expenses;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $paid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $credit;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getExpenses(): ?int
+    {
+        return $this->expenses;
+    }
+
+    public function setExpenses(int $expenses): self
+    {
+        $this->expenses = $expenses;
+
+        return $this;
+    }
+
+    public function getPaid(): ?float
+    {
+        return $this->paid;
+    }
+
+    public function setPaid(float $paid): self
+    {
+        $this->paid = $paid;
+
+        return $this;
+    }
+
+    public function getCredit(): ?int
+    {
+        return $this->credit;
+    }
+
+    public function setCredit(int $credit): self
+    {
+        $this->credit = $credit;
+
+        return $this;
+    }
+}
