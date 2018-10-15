66a407d38e8a56a15a5e9c93564d54fa317d2365
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2018-10-15 16:44:21 +0200

    Update javascript

--- assets/jsx/base.jsx
+++ assets/jsx/base.jsx
@@ -65,5 +65,57 @@ $('document').ready(function(){
         .fail(function(){
             $('#coffeelist-table').append("<div class='alert alert-success alert-dismissable' role='alert'>Could not load data from database.<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>")
         })
+
+
+
+    $('.consumer-card-buy').on('click', function(){
+        $('#buyModal').modal('show');
+        $('#buyProductModalTitle').html($('input[name=productGroup]:checked').val())
+    })
+
+})
+
+$(document).on('click', '#buyCoffee', function(event){
+    let count = $('#countOfCoffee').val()
+    let consumerId = this.attributes.getNamedItem("id").value
+    let productId = $('#buyProductId').val()
+    let value = $('#buyProductPrice').val()
+    $.ajax({
+        method: 'POST',
+        url: '/buyProduct',
+        data: {
+            count: count,
+            productId: productId,
+            consumerId: consumerId,
+            value: value
+        },
+        success: function(data){
+            console.log(data)
+            // if('success' in data.result){
+            //     $('#resultMessage').append("<div class='alert alert-success alert-dismissable' role='alert'>" +
+            //         data.result.success+
+            //         "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>")
+            //     let content = "<div class='col-lg-3' style='margin-bottom: 15px;'>" +
+            //         "<div class='consumer-card' style='width: 100%; height: 6em;'><button type='button' class='btn btn-danger removeConsumerButton' id='"+data.result.consumer.id+"' style='margin-bottom: 10px;'>Remove</button>" +
+            //         "<a class='consumerDetails-link' href='/consumers/" + data.result.consumer.id +
+            //         "' style='color: rgb(51, 51, 51);'>" +
+            //         "<div style='font-size: 18px;'>"+ data.result.consumer.firstName +" "+ data.result.consumer.lastName+"</div>";
+            //
+            //     if((data.result.consumer.expenses - data.result.consumer.paid) >= 0){
+            //         content += "<div style='font-size: 18px; color: #3f903f'>"+(data.result.consumer.expenses - data.result.consumer.paid)+ " &euro;"+
+            //             "</div>";
+            //     } else {
+            //         content += "<div style='font-size: 18px; color: #c7254e'>"+(data.result.consumer.expenses - data.result.consumer.paid)+ " &euro;"+
+            //             "</div>";
+            //     }
+            //     content += "</div></div></a></div>";
+            //     $('#consumerCards').append(content)
+            // } else {
+            //     $('#resultMessage').append("<div class='alert alert-danger alert-dismissable' role='alert'>" +
+            //         data.result.error+
+            //         "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>")
+            // }
+        }
+    })
 })
 
